eb3866551d800d581f81e9b0d37d7274e99117ec
Arghya <arghyadutta.official.1576@gmail.com>
2025-04-04 15:39:26 +0530

    Revert "Mess Up Commit"
    This reverts commit 1213f9ff1a1d83a1b32f6d01af3d1a4f03dfe8be.

--- app/src/main/java/my/first/to_dolist/MainActivity.kt
+++ app/src/main/java/my/first/to_dolist/MainActivity.kt
@@ -5,9 +5,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import my.first.to_dolist.ui.theme.ToDoListTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,7 +26,6 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ){
                     Navigation()
-                    I fuck up
                 }
             }
         }
